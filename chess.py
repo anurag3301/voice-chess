19b1f2f76a02104ccf36d54b1c55a472b00272e4
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2023-09-16 20:12:45 +0530

    Added mechanis to clear screen for each iteration and show board coordinates

--- chess.py
+++ chess.py
@@ -1,4 +1,5 @@
 from cols import *
+import os
 
 class chess:
     def __init__(self, colour):
@@ -14,10 +15,13 @@ class chess:
 
 
     def print_board(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
         for j in range(8):
+            print("  ", end="")
             for i in range(8):
                 print(f"{bg_light_green if (i+j) % 2 == 0 else bg_green}      {bg_reset}", end="")
-            print()
+
+            print(f"\n{8-j} ", end="")
             for i in range(8):
                 if self.board[j][i] is None:
                     print(f"{bg_light_green if (i+j) % 2 == 0 else bg_green}      {bg_reset}", end="")
@@ -25,13 +29,17 @@ class chess:
                 bg_col = bg_light_green if (i+j) % 2 == 0 else bg_green 
                 fg_col = text_black if self.board[j][i][0] == 'b' else text_white
                 print(f"{bg_col}{fg_col}{text_bold}   {self.board[j][i][1].upper()}  {text_reset}{bg_reset}", end="")
-            print()
+
+            print("\n  ", end="")
             for i in range(8):
                 print(f"{bg_light_green if (i+j) % 2 == 0 else bg_green}      {bg_reset}", end="")
             print()
+        print("     A     B     C     D     E     F     G     H")
 
 
 
 if __name__ == "__main__":
     c = chess("white")
-    c.print_board()
+    while True:
+        c.print_board()
+        x = input("Enter move: ")
